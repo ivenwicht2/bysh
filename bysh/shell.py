901d2e12c117eb84ef360031521e79abc01fa19c
Haytek <corentinber@wanadoo.fr>
2020-07-28 19:46:35 +0200

    oups

--- bysh/shell.py
+++ bysh/shell.py
@@ -20,7 +20,8 @@ class Shell:
     def repl_loop(self):
         while not self.store.exit:
             self.get_input()
-            if not self.current or self.current_input.isspace():
+
+            if not self.current_input or self.current_input.isspace():
                 continue
 
             self.parse_ast(self.current_input)
